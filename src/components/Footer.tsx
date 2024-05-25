95fc659f79f4c0032cbcde43bf040fd39a363b8d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-05-25 18:12:25 +0200

    update footer signature

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,30 @@
 import Link from 'next/link';
 
+import { HeartIcon } from 'lucide-react';
+
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
+    <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-20">
       <p className="text-xs text-gray-500 dark:text-gray-400">
         © {currentYear} Holidaze. All rights reserved.
       </p>
       <nav className="flex gap-4 sm:ml-auto sm:gap-6">
-        <Link className="text-xs underline-offset-4 hover:underline" href="#">
-          Terms of Service
+        <Link
+          href="https://kyrre.dev"
+          className="text-sm text-gray-500 hover:underline"
+          target="_blank"
+        >
+          Made With <HeartIcon className="inline-block size-5 fill-sky-400 stroke-none" /> by Kyrre
+          Gjerstad
         </Link>
-        <Link className="text-xs underline-offset-4 hover:underline" href="#">
-          Privacy
-        </Link>
-        <Link className="text-xs underline-offset-4 hover:underline" href="#">
-          Contact
+        <Link
+          href="https://github.com/kyrregjerstad/holidaze"
+          className="text-sm text-gray-500 hover:underline"
+          target="_blank"
+        >
+          GitHub
         </Link>
       </nav>
     </footer>
