1d7823c882aa165c69330eb634181c1e4ddf3317
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2025-01-01 14:58:03 +0100

    update analytics

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,11 +1,13 @@
 import Script from 'next/script';
 
+import { PUBLIC_PLAUSIBLE_SCRIPT_URL } from '$env/static/public';
+
 import { HotJar } from './HotJar';
 
 export const Analytics = () => {
   return (
     <>
-      <Script defer data-domain="holidaze.homes" src="https://insight.webstad.com/js/script.js" />
+      <Script defer data-domain="holidaze.homes" src={PUBLIC_PLAUSIBLE_SCRIPT_URL} />
       <HotJar />
     </>
   );
