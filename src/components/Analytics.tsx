0d62ab4276d070e9cb3dd699cc24ebc1189f10ab
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2025-01-01 14:59:13 +0100

    update env

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,13 +1,15 @@
 import Script from 'next/script';
 
-import { PUBLIC_PLAUSIBLE_SCRIPT_URL } from '$env/static/public';
-
 import { HotJar } from './HotJar';
 
 export const Analytics = () => {
   return (
     <>
-      <Script defer data-domain="holidaze.homes" src={PUBLIC_PLAUSIBLE_SCRIPT_URL} />
+      <Script
+        defer
+        data-domain="holidaze.homes"
+        src={process.env.NEXT_PUBLIC_PLAUSIBLE_SCRIPT_URL}
+      />
       <HotJar />
     </>
   );
